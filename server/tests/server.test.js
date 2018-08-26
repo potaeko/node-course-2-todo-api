eba813b840aab4c50b50e5e76fc8d0b0e9aaec67
Thomas Ko <ThomasKo@thomass-mbp.gateway.sonic.net>
2018-08-25 23:05:45 -0700

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,9 +11,21 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo')
 
-//**We will want to delete all the data in Mongodb before the test because "expect(todos[0].text).toBe(text); " we want to get the first item 
+//***To Test GET /todos
+const todos = [{
+    text : 'First test todo'
+}, {
+    text : 'Second test todo'
+}]; 
+
+
+//**To Test POST /todos We will want to delete all the data in Mongodb before the test 
+//because "expect(todos[0].text).toBe(text); " we want to get the first item 
 beforeEach((done)=>{
-    Todo.remove({}).then(()=>done());
+    Todo.remove({}).then(()=>{
+        //insertMany method from mongoose method with const todos
+        return Todo.insertMany(todos);
+    }).then(done());
 });
 
 //describe makes Headline To show in Terminal for easier to read the test
@@ -32,10 +44,10 @@ describe('Test POST /todos',()=>{
             if(err){    //handle the error
                 return done(err); //printing the error, return will stop the code here, call done for supertest
             }
-            Todo.find().then((todos)=>{ //we want to check todos with data we send
+            Todo.find({text}).then((todos)=>{ //we want to check todos with var text we send
                 expect(todos.length).toBe(1);   //should have only 1 item in todos object
-                expect(todos[0].text).toBe(text);   //the check the text we send only 1 item to be text
-                done();// call done for supertest
+                expect(todos[0].text).toBe(text);   //expect text we send only 1 item to be text
+                done();// call done for supertest ,done() for asynchronous
             }).catch((e)=>done(e));    //catch will get the error in callback, because if expect fail the test still passed
         })
     })
@@ -43,7 +55,7 @@ describe('Test POST /todos',()=>{
     //send empty object, get 400 code, assumption from database no data in todos
     it('should not create todo with invalid body data', (done)=>{
         request(app)
-        .post('/todos')
+        .post('/todos') //post data
         .send({}) //send empty object to /todos route
         .expect(400) //expect 400 bad request
         .end((err,res)=>{
@@ -52,9 +64,22 @@ describe('Test POST /todos',()=>{
             }
 
             Todo.find().then((todos)=>{
-                expect(todos.length).toBe(0); //expect empty object in todos
-                done();
+                expect(todos.length).toBe(2); //expect two objects in todos that we created as todos
+                done(); //done() for asynchronous
         }).catch((e)=>done(e))
         });
     });
+
+//GET /todos Test
+describe('GET /todos', () =>{
+    it('should get all todos', (done)=>{
+        request(app)
+        .get('/todos') //get data
+        .expect(200) //expect to get 200 code ,ok
+        .expect((res)=>{    //res.body.todos.length should be 2
+            expect(res.body.todos.length).toBe(2)
+            })
+            .end(done)
+        })
+    })
 });
