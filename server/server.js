65a23d65cb41ad9e393582dd3b2aafda035254e1
Thomas Ko <ThomasKo@thomass-mbp.gateway.sonic.net>
2018-08-27 01:05:27 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var bodyParser = require('body-parser'); //take JSON and convert to object
 
+var {ObjectID} = require('mongodb')
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {Users} =require('./models/User');
@@ -43,5 +44,30 @@ app.listen(3000, ()=>{
     console.log('Started on port 3000');
 });
 
+//GET /todos/1234567 , fetch id parameter
+app.get('/todos/:id',(req,res)=>{
+    //**get id from **params** ,caution params not param*
+    var id = req.params.id;
+    // res.send(req.params); //To test in Postman
+
+    //Valid id using isValid
+    if(!ObjectID.isValid(id)){
+        //404 - send back empty send
+        return res.status(404).send()
+    }
+    //findById
+    Todo.findById(id).then((todo)=>{
+        //if no todo - send back 404 with empty body
+        if(!todo){
+            return res.status(404).send();
+        } 
+        //success - send back todo
+        res.send({todo}) //todo:todo
+        //error case ,400 - send empty body back   
+        }).catch((e)=>{
+            res.status(400).send();
+        })
+    });
+        
 
 module.exports = {app};
